883af12cb80edcf202ab5880d4eed94b8768253b
Vedant Jha <6vedant@gmail.com>
2022-09-07 22:04:19 +0530

    Touch effect added to buttons

--- Sources/PinAuthApp/main.page.swift
+++ Sources/PinAuthApp/main.page.swift
@@ -2,6 +2,10 @@ import ScadeKit
 
 class MainPageAdapter: SCDLatticePageAdapter {
 
+  // colors
+  let colorDefaultGreen = SCDGraphicsRGB(red: 211, green: 211, blue: 211, alpha: 255)
+  let whiteColor = SCDGraphicsRGB(red: 255, green: 255, blue: 255, alpha: 255)
+
   // page adapter initialization
   //position initialization
   var position = 0
@@ -12,43 +16,21 @@ class MainPageAdapter: SCDLatticePageAdapter {
   override func load(_ path: String) {
     super.load(path)
 
-    self.b0.onClick { _ in
-      self.updatePin(val: 0)
-    }
+    // add gesture to button. Is changes the background color
+    // of the button when button is pressed (began) and finger is lifted up (ended)
+    self.b0.drawing!.gestureRecognizers.append(getUpDownGestureForButton(btn: self.b0, val: 0))
+    self.b1.drawing!.gestureRecognizers.append(getUpDownGestureForButton(btn: self.b1, val: 1))
+    self.b2.drawing!.gestureRecognizers.append(getUpDownGestureForButton(btn: self.b2, val: 2))
+    self.b3.drawing!.gestureRecognizers.append(getUpDownGestureForButton(btn: self.b3, val: 3))
+    self.b4.drawing!.gestureRecognizers.append(getUpDownGestureForButton(btn: self.b4, val: 4))
+    self.b5.drawing!.gestureRecognizers.append(getUpDownGestureForButton(btn: self.b5, val: 5))
+    self.b6.drawing!.gestureRecognizers.append(getUpDownGestureForButton(btn: self.b6, val: 6))
+    self.b7.drawing!.gestureRecognizers.append(getUpDownGestureForButton(btn: self.b7, val: 7))
+    self.b8.drawing!.gestureRecognizers.append(getUpDownGestureForButton(btn: self.b8, val: 8))
+    self.b9.drawing!.gestureRecognizers.append(getUpDownGestureForButton(btn: self.b9, val: 9))
+    self.b_cancel.drawing!.gestureRecognizers.append(
+      getUpDownGestureForButton(btn: self.b_cancel, val: -1))
 
-    self.b1.onClick { _ in
-      self.updatePin(val: 1)
-    }
-
-    self.b2.onClick { _ in
-      self.updatePin(val: 2)
-    }
-
-    self.b3.onClick { _ in
-      self.updatePin(val: 3)
-    }
-    self.b4.onClick { _ in
-      self.updatePin(val: 4)
-    }
-    self.b5.onClick { _ in
-      self.updatePin(val: 5)
-    }
-    self.b6.onClick { _ in
-      self.updatePin(val: 6)
-    }
-    self.b7.onClick { _ in
-      self.updatePin(val: 7)
-    }
-    self.b8.onClick { _ in
-      self.updatePin(val: 8)
-    }
-    self.b9.onClick { _ in
-      self.updatePin(val: 9)
-    }
-
-    self.b_cancel.onClick { _ in
-      self.updatePin(val: -1)
-    }
   }
 
   func updatePin(val: Int) {
@@ -99,6 +81,32 @@ class MainPageAdapter: SCDLatticePageAdapter {
 
   func callFinalMethod(pin: String) {
     print("Final 5-digit pincode: \(pin)")
+
+  }
+
+  func getUpDownGestureForButton(btn: SCDWidgetsButton, val: Int) -> SCDSvgPanGestureRecognizer {
+
+    // Create action*
+    func onPanAction(recognizer: SCDSvgGestureRecognizer?) {
+
+      // depending on whether we are inside or outside of the button,
+      // we set the button background a different color
+      switch recognizer!.state {
+      case .began:
+        btn.backgroundColor = self.colorDefaultGreen
+        self.updatePin(val: val)
+      case .ended:
+        btn.backgroundColor = self.whiteColor
+      default:
+        return
+      }
+    }
+
+    // create recognizer
+    let panGestureRecognizer = SCDSvgPanGestureRecognizer(onPanAction)
+
+    // Configure gesture --> nothing to configure. Return it
+    return panGestureRecognizer
 
   }
 
